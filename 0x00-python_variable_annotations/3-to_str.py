f3f5973059995ef4d12d062bdc232e009928f013
godfreydekew <dekewgodfrey@gmail.com>
2024-07-07 13:28:32 +0000

    Task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''A function float to str'''
+
+
+def to_str(n: float) -> str:
+    """
+    Args:
+        n (float): number to convert
+
+    Returns:
+        str: converted string
+    """
+    return str(n)
