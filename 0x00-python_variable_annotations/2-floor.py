4a2f405abe35a14cc76d5d87afa5223e2bc63ff6
godfreydekew <dekewgodfrey@gmail.com>
2024-07-07 13:15:36 +0000

    Task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''A fucntion to calculate the floor'''
+
+
+def floor(n: float) -> float:
+    """
+    Args:
+        n (float): number to floor
+
+    Returns:
+        float: result of floor
+    """
+    return int(n)
