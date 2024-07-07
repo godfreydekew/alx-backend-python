b774310c7c024df5a5c63f29f50689ba7004599c
godfreydekew <dekewgodfrey@gmail.com>
2024-07-07 12:56:01 +0000

    Task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+'''Add fucntion'''
+
+
+def add(a: float, b: float) -> float:
+    """
+    Args:
+        a (float): any float
+        b (float): any float
+
+    Returns:
+        float: sum of the values
+    """
+    return a + b
