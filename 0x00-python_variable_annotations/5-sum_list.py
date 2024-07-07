2a902e12afdb2db796e0908396b2f8b1e57a1ab3
godfreydekew <dekewgodfrey@gmail.com>
2024-07-07 14:13:40 +0000

    Task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''Calculates usm of a list'''
+
+
+def sum_list(input_list: list[float]) -> float:
+    """
+    Args:
+        input_list (list[float]): list of floats
+
+    Returns:
+        float: sum of floats
+    """
+    return sum(input_list)
