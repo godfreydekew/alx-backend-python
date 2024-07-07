5ac34c1991537ed40099185d60a883d8abce3888
godfreydekew <dekewgodfrey@gmail.com>
2024-07-07 16:02:46 +0000

    Replace Nonetype with None

--- 0x00-python_variable_annotations/100-safe_first_element.py
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -1,12 +1,12 @@
 #!/usr/bin/env python3
 '''duck-typed annotations'''
 import typing
-from types import NoneType
 
 
 # The types of the elements of the input are not know
 def safe_first_element(lst: typing.Sequence[typing.Any]
-                       ) -> typing.Union[typing.Any, NoneType]:
+                       ) -> typing.Union[typing.Any, None]:
+    """A function to return first element of a sequence"""
     if lst:
         return lst[0]
     else:
