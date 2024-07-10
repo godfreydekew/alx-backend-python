41bb50f4342ffc7e7d4aefa1cba552bd38bf4a52
godfreydekew <dekewgodfrey@gmail.com>
2024-07-10 09:08:56 +0000

    Add comments aftyer the shebang

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python3
+'''asynchronous coroutine '''
+
 import asyncio
 import random
-'''asynchronous coroutine '''
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    '''A asynchronous coroutine that awaits  for a random delay'''
+    '''
+    A asynchronous coroutine that awaits  for a random delay
+    '''
     i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
+
     return i
