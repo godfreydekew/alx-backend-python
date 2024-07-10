93111bae84cdb10bb10315b4efa6f29d66ef9781
godfreydekew <dekewgodfrey@gmail.com>
2024-07-10 09:03:58 +0000

    Add float to return type

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -4,7 +4,7 @@ import random
 '''asynchronous coroutine '''
 
 
-async def wait_random(max_delay: int = 10):
+async def wait_random(max_delay: int = 10) -> float:
     '''A asynchronous coroutine that awaits  for a random delay'''
     i = random.uniform(0, max_delay)
     await asyncio.sleep(i)
