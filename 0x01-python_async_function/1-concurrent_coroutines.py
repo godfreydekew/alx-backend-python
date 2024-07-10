2b901dc723b454997143f559e7d5bbb15a7a2ba1
godfreydekew <dekewgodfrey@gmail.com>
2024-07-10 16:00:07 +0000

    Task 1

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+'''
+A function which spawns wait_random
+'''
+
+import asyncio
+from typing import List
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    '''
+    Creates n tasks that wait for a random time between 0 and max_delay.
+    '''
+    tasks = await asyncio.gather(*[wait_random(max_delay) for _ in range(n)])
+
+    return tasks
