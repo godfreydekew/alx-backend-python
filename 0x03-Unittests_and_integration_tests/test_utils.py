de51c8dc63629c60c508cb14059e7fd0eacf2bb0
godfreydekew <dekewgodfrey@gmail.com>
2024-07-30 15:10:08 +0000

    Task 3

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -3,7 +3,7 @@
 """
 from parameterized import parameterized
 from typing import Dict, Tuple, Union
-from utils import access_nested_map, get_json
+from utils import access_nested_map, get_json, memoize
 import unittest
 from unittest.mock import patch, Mock
 
@@ -54,3 +54,28 @@ class TestGetJson(unittest.TestCase):
             result = get_json(test_url)
             mk_get.assert_called_once_with(test_url)
             self.assertEqual(result, test_payload)
+
+
+class TestMemoize(unittest.TestCase):
+    """Tests the memoize decorator."""
+
+    def test_memoize(self):
+        """Tests that a_method is called only once with memoization."""
+        class TestClass:
+
+            def a_method(self):
+                return 42
+
+            @memoize
+            def a_property(self):
+                return self.a_method()
+
+        test_instance = TestClass()
+
+        with patch.object(test_instance,
+                          'a_method',
+                          wraps=test_instance.a_method
+                          ) as mock_method:
+            self.assertEqual(test_instance.a_property, 42)
+            self.assertEqual(test_instance.a_property, 42)
+            mock_method.assert_called_once()
